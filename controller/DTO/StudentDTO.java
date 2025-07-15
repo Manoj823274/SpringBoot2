020bb0b85c97b8d41f30b3f455ed32310cfa3aa4
Manoj823274 <basanimanoj65@gmail.com>
2025-07-15 12:48:12 +0530

    Add files via upload

---
+++ controller/DTO/StudentDTO.java
@@ -0,0 +1,13 @@
+
+package com.example.school.dto;
+
+import lombok.*;
+import java.time.LocalDate;
+
+@Getter @Setter
+public class StudentDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+}

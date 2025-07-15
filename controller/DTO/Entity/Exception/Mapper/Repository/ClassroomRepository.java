9c2c255a28e2a35daa59a53e02b6a0644da72a28
Manoj823274 <basanimanoj65@gmail.com>
2025-07-15 13:01:40 +0530

    Add files via upload

---
+++ controller/DTO/Entity/Exception/Mapper/Repository/ClassroomRepository.java
@@ -0,0 +1,7 @@
+
+package com.example.school.repository;
+
+import com.example.school.entity.Classroom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassroomRepository extends JpaRepository<Classroom, Long> {}
